13081a6ce51acdf2927e2d2a913bc090e12e3bce
Muzammil98 <muzammilsarwar0@gmail.com>
2020-04-01 20:57:41 +0500

    Renamed app

--- android/app/src/main/java/com/reduxnative/MainActivity.java
+++ android/app/src/main/java/com/reduxnative/MainActivity.java
@@ -1,4 +1,4 @@
-package com.with_redux;
+package com.reduxnative;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "with_redux";
+    return "ReduxNative";
   }
 }
--- ios/ReduxNativeTests/ReduxNativeTests.m
+++ ios/ReduxNativeTests/ReduxNativeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface with_reduxTests : XCTestCase
+@interface ReduxNativeTests : XCTestCase
 
 @end
 
-@implementation with_reduxTests
+@implementation ReduxNativeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
